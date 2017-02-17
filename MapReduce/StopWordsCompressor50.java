e18056672a3d63d36c614d647ba9c0543501180d
siham-ot <siham.ouazzani@essec.edu>
2017-02-17 03:47:23 +0100

    Add files via upload

---
+++ MapReduce/StopWordsCompressor50.java
@@ -0,0 +1,104 @@
+package edu.stanford.cs246.wordcount;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.apache.log4j.Logger;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*; 
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.GzipCodec;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+import edu.stanford.cs246.wordcount.WordCount.Map;
+import edu.stanford.cs246.wordcount.WordCount.Reduce;
+
+public class StopWordsCompressor50 extends Configured implements Tool {
+	  public static void main(String[] args) throws Exception {
+		    int res = ToolRunner.run(new StopWordsCompressor50(), args);
+		    System.exit(res);
+		  }
+	  
+	  @Override
+	   public int run(String[] args) throws Exception {
+	      System.out.println(Arrays.toString(args));
+	      Job job = new Job(getConf(), "StopWordsCompressor50");
+	      Configuration Conf = new Configuration();
+	      
+	      job.setJarByClass(StopWordsCompressor50.class);
+	      job.setOutputKeyClass(Text.class);
+	      job.setOutputValueClass(IntWritable.class);
+
+	      job.setMapperClass(Map.class);
+	      job.setCombinerClass(Reduce.class);
+	      job.setReducerClass(Reduce.class);
+
+	      job.setInputFormatClass(TextInputFormat.class);
+	      job.setOutputFormatClass(TextOutputFormat.class);
+	      
+	      job.getConfiguration().set(
+				"mapreduce.output.textoutputformat.separator", ",");
+		  job.setNumReduceTasks(50);
+		  
+		  
+		  Conf.setBoolean("mapreduce.map.output.compress",true);
+		  Conf.set("mapred.map.output.compression.codec","org.apache.hadoop.io.compress.SnappyCodec");
+		  
+		  
+	      FileInputFormat.addInputPath(job, new Path(args[0]));
+	      FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		  
+		  
+		  FileSystem txtfile = FileSystem.newInstance(getConf());
+	      if (txtfile.exists(new Path(args[1]))) {
+	    	  txtfile.delete(new Path(args[1]), true);
+			}
+
+	      return job.waitForCompletion(true) ? 0 : 1;
+	   }
+	  
+
+public static class Map extends Mapper<LongWritable, Text, Text, IntWritable> {
+		private final static IntWritable ONE = new IntWritable(1);
+		private Text word = new Text();
+
+		@Override
+		public void map(LongWritable key, Text value, Context context)
+				throws IOException, InterruptedException {
+			for (String token : value.toString().split("\\s+")) {
+				word.set(token.toLowerCase());
+				context.write(word, ONE);
+			}
+		}
+	}
+	  
+public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+	@Override
+	public void reduce(Text key, Iterable<IntWritable> values,
+			Context context) throws IOException, InterruptedException {
+		int sum = 0;
+		for (IntWritable val : values) {
+			sum += val.get();
+		}
+		if (sum > 4000) {
+			context.write(key, new IntWritable(sum));
+		}
+	}
+}
+
+
+
+}
